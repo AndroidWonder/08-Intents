26a3e83501f84d721194b63cf676da5d7d5e5392
ClassroomAccount <jpepe44@gmail.com>
2019-01-31 19:42:59 -0500

    fix UI errors

--- app/src/main/java/com/course/example/intents/LifeCycleTest.java
+++ app/src/main/java/com/course/example/intents/LifeCycleTest.java
@@ -20,7 +20,7 @@ public class LifeCycleTest extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.lifecycle);
 
 		txtToDo = (TextView) findViewById(R.id.txtToDo);
 		txtMsg = (EditText) findViewById(R.id.txtMsg);
